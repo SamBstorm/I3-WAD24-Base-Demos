c22c40f98845a42650dbbb2738e09559044b1c18
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-04 12:18:37 +0200

    Add Demo03 - Convertion

---
+++ Demo03/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Demo03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*DateTime today  = DateTime.Now;
+            Console.WriteLine(today.ToString());
+            Console.WriteLine(today.ToString("yyyy MMMM dd"));*/
+
+            /* //Avec Parse
+            Console.WriteLine("Quel jour sommes-nous?");
+            string userAnswer = Console.ReadLine();
+
+            int dayInMonth = int.Parse(userAnswer);
+
+            Console.WriteLine($"Ok, il reste donc +/- {31 - dayInMonth} jours dans le mois...");*/
+
+            //Avec TryParse
+            Console.WriteLine("Quel jour sommes-nous?");
+            string userAnswer = Console.ReadLine();
+
+            int dayInMonth;
+            bool isConverted;
+
+            isConverted = int.TryParse(userAnswer, out dayInMonth);
+
+            Console.WriteLine($"Convertion réussie ? {isConverted}");
+            Console.WriteLine($"Ok, il reste donc +/- {31 - dayInMonth} jours dans le mois...");
+        }
+    }
+}
