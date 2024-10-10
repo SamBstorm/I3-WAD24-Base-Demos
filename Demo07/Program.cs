3f9ffc23f37e642cac1ea161d2952a85c296db33
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-10 14:49:17 +0200

    Add Demo07 & Demo08 : Boucles

---
+++ Demo07/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Demo07
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/* Version WHILE
+			Console.WriteLine("Veuillez introduire votre BBAN (12 chiffres) :");
+			string userInput = Console.ReadLine();	//Initialise
+			while (userInput.Length != 12 || !long.TryParse(userInput, out _))	//Condition
+			{
+				Console.WriteLine("Entrée invalide");
+				userInput = Console.ReadLine();		//Modification (itération)
+			}
+			Console.WriteLine("Entrée valide"); */
+
+			/*Version DO...WHILE*/
+			string message = "Veuillez introduire votre BBAN (12 chiffres) :";
+			string userInput;	//Déclaration (ATTENTION pas initialisation)
+			do
+			{
+				Console.WriteLine(message);
+				userInput = Console.ReadLine(); //Initialisation (1ère fois)
+												//Modification (fois suivantes)
+				message = "Entrée invalide";
+			} while (userInput.Length != 12 || !long.TryParse(userInput, out _));
+			Console.WriteLine("Entrée valide");
+		}
+	}
+}
---
+++ Demo08/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo08
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const string PASSWORD = "Test1234=";
+			int countdown = 3;
+			string password;
+			do
+			{
+				Console.WriteLine($"Veuillez introduire votre mot de passe : ({countdown} essais restant)");
+				password = Console.ReadLine();
+				countdown--;
+			} while (password != PASSWORD && countdown > 0);
+
+			if(password is PASSWORD)
+			{
+				Console.WriteLine("Bienvenue!");
+			}
+			else
+			{
+				Console.WriteLine("Compte bloqué!");
+			}
+		}
+	}
+}
