8283c548812906bcfca3b0386bc2819da57bd05a
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-11 10:15:25 +0200

    Add Demo09 - Random

---
+++ Demo09/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo09
+{
+	internal class Program
+	{
+		/*static Random RNG = new Random(); //Plus accessible en Orienté Objet*/
+		static void Main(string[] args)
+		{
+			Random RNG = new Random();
+			for (int nbLance = 0; nbLance < 2; nbLance++)
+			{
+				Console.WriteLine($"Un dé m'a sorti : {RNG.Next(1, 7)}"); //Valeur aléatoire entre 1 et 6 (7 non-inclu)
+			}
+		}
+	}
+}
