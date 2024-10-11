d220a7da20b46be928fde3260cea94c985491a9e
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-11 15:44:47 +0200

    Add Demo10 - Tableau

---
+++ Demo10/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Demo10
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] dayNames = { 
+				"dimanche", 
+				"lundi",
+				"mardi",
+				"mercredi",
+				"jeudi",
+				"vendredi",
+				"samedi"
+			};
+			/* Aussi déclarable avec les  [] 
+			 string[] dayNames = [ 
+				"dimanche", 
+				"lundi",
+				"mardi",
+				"mercredi",
+				"jeudi",
+				"vendredi",
+				"samedi"
+			];
+			 */
+
+			Console.WriteLine($"Il y a {dayNames.Length} jours de la semaine.");
+
+			foreach (string dayName in dayNames) {
+				Console.WriteLine($"\t- {dayName}");
+			}
+
+			/*Console.WriteLine("En anglais, comment traduisez-vous :");
+			for (int i = 0; i < dayNames.Length; i++) {
+				Console.Write($"\t- {dayNames[i]}\t:");
+				dayNames[i] = Console.ReadLine();
+			}*/
+
+			Console.WriteLine($"Aujourd'hui nous sommes {dayNames[(int)DateTime.Now.DayOfWeek]} le {DateTime.Now.ToString("dd MMMM yyyy")}.");
+		}
+	}
+}
