3c0cbd1ee8914e8149defec787aab2c73eb8c132
Samuel Legrain <samuel.legrain@bstorm.be>
2024-11-25 12:08:38 +0100

    Add Demo15 : methods

---
+++ Demo15/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ToolBox tb = new ToolBox();
+
+            string text = null;
+
+            tb.WriteVerticaly("Salut les WADs!");
+            tb.WriteVerticaly(text);
+            
+            tb.WriteVerticaly(new char[] { 'O', 'K' });
+            tb.WriteVerticaly(['\0', ' ', '\t']);
+        }
+    }
+}
---
+++ Demo15/Structs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo15
+{
+    public struct ToolBox
+    {
+        public void WriteVerticaly(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                return; //La meilleur solution serait d'utiliser des exceptions (Vu en OO)
+            }
+            char[] chars = message.ToCharArray();
+            //foreach (char c in chars)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            WriteVerticaly(chars);
+        }
+
+        public void WriteVerticaly(char[] message)
+        {
+            if (
+                message is null 
+                || message.Length == 0 
+                || CharArrayContainsOnlyWhiteSpace(message)
+                ) {
+                return; //La meilleur solution serait d'utiliser des exceptions (Vu en OO)
+            }
+            foreach (char c in message)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        public bool CharArrayContainsOnlyWhiteSpace(char[] array)
+        {
+            foreach (char c in array)
+            {
+                if(!char.IsWhiteSpace(c) && c != '\0')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
