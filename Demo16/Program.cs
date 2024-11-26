7971b189a0934b7f80d989f979324edb571a8ebb
Samuel Legrain <samuel.legrain@bstorm.be>
2024-11-26 11:32:35 +0100

    Add Demo16 : Enum Trafficlight

---
+++ Demo16/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrafficLight feu_rouge = new TrafficLight();
+            feu_rouge.color = TrafficLightColor.Green;
+            Console.WriteLine($"Presently, the traffic light is {feu_rouge.color}.");
+            feu_rouge.ChangeColor();
+            Console.WriteLine($"Presently, the traffic light is {feu_rouge.color}.");
+            feu_rouge.ChangeColor();
+            Console.WriteLine($"Presently, the traffic light is {feu_rouge.color}.");
+        }
+    }
+}
